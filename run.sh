a8e9f90ba506f63c78a369e0900db5934f3d344e
Corteggiani <n.corteggiani@gmail.com>
2020-09-23 19:57:52 +0200

    Comment CGU_Init for testing unsing partial-rehosting

--- run.sh
+++ run.sh
@@ -3,17 +3,13 @@
 #rm -rf build && mkdir build
 cd build && cmake -DCMAKE_BUILD_TYPE=Debug .. && make clean && make
 
-exit
-
 reset_handler=$(readelf -a ./baremetal.$1 | grep "Reset_Handler" | awk '{print $2}')
 
-arm-none-eabi-objcopy --set-start 0x1c000000 ./baremetal.$1 -O binary ./baremetal.$1.bin
+arm-none-eabi-objcopy --set-start 0x1c000000 ./baremetal.$1 -O binary ./callee.bin
 
 arm-none-eabi-objdump -S -marm -D ./baremetal.$1 > ./baremetal.$1.dis
 
 readelf -S ./baremetal.$1
-
-exit
 
 echo "h" > flash.jlink
 echo "loadbin ./baremetal."$1".bin 1C000000" >> flash.jlink
